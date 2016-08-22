316010e60c6aa717241d80ba0a7dbe88ea225150
Alex Cristea <alex@thinslices.com>
2016-08-22 10:06:56 +0300

    Setup framework targets

---
+++ framework/Sources/ValidationComponents iOS.h
@@ -0,0 +1,19 @@
+//
+//  ValidationComponents iOS.h
+//  ValidationComponents iOS
+//
+//  Created by Alex Cristea on 22/08/16.
+//  Copyright © 2016 iOS NSAgora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ValidationComponents iOS.
+FOUNDATION_EXPORT double ValidationComponents_iOSVersionNumber;
+
+//! Project version string for ValidationComponents iOS.
+FOUNDATION_EXPORT const unsigned char ValidationComponents_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidationComponents_iOS/PublicHeader.h>
+
+
